1f62c66a2ec22215476e23515c012b183b4b1625
dhkdtld37 <hyeonls96@naver.com>
2025-11-07 01:00:20 +0900

    마이페이지 면접후기 아이콘 수정

--- app/mypage/MyPageClient.tsx
+++ app/mypage/MyPageClient.tsx
@@ -91,7 +91,8 @@ export default function MyPageClient({ user, profile, posts, comments, interview
                   >
                     <div className="w-5 h-5 mr-3 flex items-center justify-center">
                       <svg className="w-5 h-5 text-orange-600 dark:text-orange-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2-2v2m8 0V6a2 2 0 012 2v6a2 2 0 01-2 2H6a2 2 0 01-2-2V8a2 2 0 012-2V6" />
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 3h6a2 2 0 012 2v14a2 2 0 01-2 2H9a2 2 0 01-2-2V5a2 2 0 012-2z" />
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 7h6M9 11h6M9 15h6M7 11h.01M7 15h.01" />
                       </svg>
                     </div>
                     면접 후기
